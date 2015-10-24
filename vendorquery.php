101bf97617f4fa2dd04374bc19672b31855c068c
Beatrice Huang <beahuang1@gmail.com>
2015-10-24 23:05:11 +1100

    add in vendor querying

--- vendorquery.php
+++ vendorquery.php
@@ -1,4 +1,11 @@
 <?php
 	session_start();
 	$orderType = sanitise_input($_POST["orderType"]);
+	$nameKey = sanitise_input($_POST["name"]);
+	$productKey = sanitise_input($_POST["product"]);
+	$updateOrderNumber = sanitise_input($_POST["updateOrderNumber"]);
+	$updateOrderStatus = sanitise_input($_POST["orderStatus"]);
+	$deleteOrderNumber = sanitise_input($_POST["deleteOrderNumber"]);
+
+	echo "$orderType $nameKey $productKey $updateOrderNumber $updateOrderStatus $deleteOrderNumber";
 ?>
